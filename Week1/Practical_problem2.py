10849fda80240ec28fffbf1b48b7c5117682e795
Edgar-creator <edgarbabayan374@gmail.com>
2020-03-07 12:56:14 +0400

    second

---
+++ Week1/Practical_problem2.py
@@ -0,0 +1,11 @@
+#Problem 2
+course = "Intro To Python"
+student = "Edgar Babayan"
+nickname = "Aristocrat"
+grade = 15
+python_expert = True
+print(course)
+print(student)
+print(nickname)
+print(grade)
+print(python_expert)
---
+++ Week1/test.py
@@ -0,0 +1,17 @@
+#Problem 2
+course = "Intro To Python"
+student = "Edgar Babayan"
+nickname = "Aristocrat"
+grade = 15
+python_expert = True
+print(course)
+print(student)
+print(nickname)
+print(grade)
+print(python_expert)
+
+#
+AB = 3
+AC = 4
+BC = (AB ** 2 + AC ** 2) ** 0.5
+print("The hypotenuse of the triangle ABC" + '=', BC) 
