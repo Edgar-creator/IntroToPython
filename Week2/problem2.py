f718654c9551f15cdc6697c5c8e0a6e7502dac11
Edgar-creator <edgarbabayan374@gmail.com>
2020-04-04 01:14:30 +0400

    add

--- Week2/problem2.py
+++ Week2/problem2.py
@@ -1,6 +1,19 @@
-num1 = int(input("number "))
-num2 = int(input("number "))
-sum = num1 + num2
-print("First number:",num1)
-print("Second number:",num2)
-print("Sum:",sum)
+str = input().lower().split(" ")
+d = {}
+l = {}
+s = {}
+for i in str:
+    if i in d:
+        d[i].append(i)
+    else:
+        d[i] =[i]
+for i in d:
+    l[i] = len(d[i])
+for key in l.keys():
+    if l[key] in s:
+        s[l[key]].append(key)
+    else:
+        s[l[key]] = [key]
+n = max(s.keys())
+for i in s[n]:
+    print(i,n)
