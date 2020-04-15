b82e23a4f6e816d385a6e4df7e5b22cc99c7f404
Edgar-creator <edgarbabayan374@gmail.com>
2020-04-15 22:25:08 +0400

    p

--- Week5/Practical/calc.py
+++ Week5/Practical/calc.py
@@ -1,14 +1,12 @@
-#Problem7
 import pretty_print
 def calculate_cube(x):
-    return x ** 3
+    return x **3
 def calculate_square(x):
     return x ** 2
 def main():
-    x = int(input())
-    result = calculate_square(x)
-    pretty_print.simple_print(result)
-    result = calculate_cube(x)
-    pretty_print.pro_print(result)
+    a = calculate_square(2)
+    pretty_print.simple_print(a)
+    a = calculate_cube(4)
+    pretty_print.pro_print(a)
 if __name__ == "__main__":
     main()
--- Week5/Practical/pretty_print.py
+++ Week5/Practical/pretty_print.py
@@ -1,4 +1,4 @@
 def simple_print(x):
     print("Result:%d" %(x))
 def pro_print(x):
-    print("The result of theoperation is:%d" %(x))
+    print("The result of theoperation is %d" %(x))
