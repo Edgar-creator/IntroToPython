93fa842383c57e778d7f6b307fd64378a8314b91
Edgar-creator <61898478+Edgar-creator@users.noreply.github.com>
2020-04-15 22:11:16 +0400

    Delete Productcheck.py

--- Week5/Homework/Productcheck.py
+++
@@ -1,6 +0,0 @@
-products = {"candy": 10, "juice": 5, "pen": 50}
-def check(product,num):
-    if product in products and products[product] == num:
-        return True
-    else:
-        return False
